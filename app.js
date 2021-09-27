6135adefea2cb03467f1c19e6aab4ae8694c9e99
mickym23 <mikhail.cedras@gmail.com>
2021-09-27 18:50:10 +0200

    code

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'css')));
+app.use(express.static(path.join(__dirname, 'img')));
 
 const navList = [{
    'linkName': 'Home',
    'href': '/'
+},{
+   'linkName': 'Book Display',
+   'href': '/display'
 },{
    'linkName': 'Add Product',
    'href': '/add-product'
 }];
 
 
-const books = ['Book 1','Book 2', 'Book 3', 'Book 4'];
+const books = ['Book 1', 'Book 2', 'Book 3', 'Book 4'];
 
 app.get('/add-product', (req, res, next) => {
    res.render('add-product', {
@@ -34,11 +38,18 @@ app.post('/add-item', (req, res, next) => {
    res.redirect('/');
 });
 
-app.get('/', (req, res, next) => {
-   res.render('home',{
+app.get('/display', (req, res, next) => {
+   res.render('display',{
       pageTitle: 'Booklist | Node',
       books:books,
-      'navList':navList
+      navList:navList
+      });
+   });
+
+app.get('/', (req, res, next) => {
+   res.render('home',{
+      pageTitle: 'Home | Node',
+      navList:navList
    });
 });
 
