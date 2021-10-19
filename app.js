63230860a4ed442798decf9831557e2e4afa98b4
mickym23 <mikhail.cedras@gmail.com>
2021-10-19 11:15:39 +0200

    Added Prove04 styles

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+// Required NPM Packages
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 
+// Required Models
+const User = require('./models/user');
+
+// MongoDB Connection string and Config Var
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://mikhail-node:Porkchops1@cluster0.mszib.mongodb.net/shop?authSource=admin&replicaSet=atlas-vg9xcm-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 
+// Create Express server
 const app = express();
 
+// Using EJS template
 app.set('view engine', 'ejs');
 
+// BodyParser and static paths to folders 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'css')));
 app.use(express.static(path.join(__dirname, 'img')));
@@ -21,7 +29,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const User = require('./models/user');
+// User Middleware (finding hard-coded user)
 app.use((req, res, next) => {
    User.findById('616e1954aee925b44b6ce3ee')
    .then(user => {
@@ -37,25 +45,29 @@ const options = {
    autoIndex: false
 };
 
-                       
+// PORT for Heroku or Localhost
 const PORT = process.env.PORT || 3000;
 
+// Shop Routes
 const shopRoutes = require('./routes/shop');
 app.use(shopRoutes);
 
+// Admin Routes
 const adminRoutes = require('./routes/admin');
 app.use(adminRoutes);
 
+// Error 404
 const errorController = require('./controllers/error');
 app.use(errorController.get404);
 
+// Connect to db via Mongoose
 mongoose.connect(
    MONGODB_URL, options
 )
 .then(result => {
-
    User.findOne().then(user => {
-      if(!user) {
+      if (!user) {
+         // Create a user if user is not found
          const user = new User({
             name: 'Mikhail',
             email: 'test@gmail.com',
@@ -66,10 +78,9 @@ mongoose.connect(
          user.save(); 
       }
    })
-   
+   // Connected to Port
    console.log('Connected!')
    app.listen(PORT);
-  
 })
 .catch(err => {
    console.log(err);
